480f45794c5adc1dc932c1dd19d38d4e101d59e3
raghav582 <raghavsharma582@gmail.com>
2020-05-11 13:43:25 +0530

    browser_automation.py

--- browser_automation.py
+++ browser_automation.py
@@ -7,15 +7,12 @@ Created on Fri May  8 15:00:07 2020
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 browser = webdriver.Chrome('chromedriver_linux64/chromedriver')
-
-browser.get("https://www.bseindia.com/eqstreamer/StreamerMarketwatch.html?flag=1")
-
-# locator
-link = browser.find_element_by_xpath('//*[@id="btndwnload"]')
-link.click()
-
-download_csv = browser.find_element_by_xpath('//*[@id="ContentPlaceHolder1_imgDownload"]')
-download_csv.click()
+browser.get("https://www.nseindia.com/market-data/live-equity-market")
+time.sleep(20)
+download_csv = browser.find_element_by_link_text("Download (.csv)")
+ActionChains(browser).move_to_element(download_csv).click(download_csv).perform()
+# browser.execute_script('javascript:dnldEquityStock()')
