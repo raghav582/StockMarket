4ca72f9af90f528d6c5e51375d918ba115f9453b
raghav582 <raghavsharma582@gmail.com>
2020-05-20 10:49:45 +0530

    waitE

--- browser_automation.py
+++ browser_automation.py
@@ -18,33 +18,34 @@ import pandas as pd
 class Browser_Automation:
     def __init__(self):
         self.equity_data = pd.DataFrame()
+        self.browser = None
 
     def load_browser(self):
         options = webdriver.chrome.options.Options()
         options.add_argument("user-agent = Chrome/33.0.1750.517")
 
-        browser = webdriver.Chrome('chromedriver_linux64/chromedriver', chrome_options=options)
-        agent = browser.execute_script('return navigator.userAgent')
+        self.browser = webdriver.Chrome('chromedriver_linux64/chromedriver', chrome_options=options)
+        agent = self.browser.execute_script('return navigator.userAgent')
         print(agent)
-        browser.get("https://www.nseindia.com")
+        self.browser.get("https://www.nseindia.com")
 
         # while(waitE.visibility_of_element_located((By.TAG_NAME, 'table'))):
         #     time.sleep(2)
 
-        market_data = browser.find_element_by_xpath('//*[@id="main_navbar"]/ul/li[2]/a')
-        ActionChains(browser).move_to_element(market_data).perform()
+        market_data = self.browser.find_element_by_xpath('//*[@id="main_navbar"]/ul/li[2]/a')
+        ActionChains(self.browser).move_to_element(market_data).perform()
 
         time.sleep(2)
-        equity = browser.find_element_by_link_text('Equity & SME Market')
+        waitE.visibility_of_element_located((By.LINK_TEXT, 'Equity & SME Market'))
+        equity = self.browser.find_element_by_link_text('Equity & SME Market')
         equity.click()
 
-        time.sleep(2)
-        browser.delete_all_cookies()
-        browser.refresh()
+        self.browser_refresh()
 
         time.sleep(5)
-        head = browser.find_element_by_xpath('//*[@id="equityStockTable"]/thead/tr')
-        rows = browser.find_elements_by_xpath('//*[@id="equityStockTable"]/tbody/tr')
+        waitE.visibility_of_element_located((By.XPATH, '//*[@id="equityStockTable"]/tbody/tr[51]'))
+        head = self.browser.find_element_by_xpath('//*[@id="equityStockTable"]/thead/tr')
+        rows = self.browser.find_elements_by_xpath('//*[@id="equityStockTable"]/tbody/tr')
         self.equity_data = self.equity_data.append(pd.DataFrame(head.text.split(" ")).T)
         time.sleep(2)
 
@@ -53,3 +54,8 @@ class Browser_Automation:
 
     def get_data(self):
         return self.equity_data
+
+    def browser_refresh(self):
+        time.sleep(2)
+        self.browser.delete_all_cookies()
+        self.browser.refresh()
--- stock_market.py
+++ stock_market.py
@@ -5,8 +5,17 @@ Created on 14/05/20
 """
 
 from browser_automation import Browser_Automation
+import pandas as pd
+import datetime
 
 BA_obj = Browser_Automation()
 BA_obj.load_browser()
 equity_data = BA_obj.get_data()
+
+while(equity_data.any().isnull().any()):
+    BA_obj.browser_refresh()
+    equity_data = BA_obj.get_data()
+
+file_path = 'datasets/' + str(datetime.date.today()) + '.csv'
+equity_data.to_csv(file_path)
 print(equity_data.head())
