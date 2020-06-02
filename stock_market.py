9cb37c0d50cdbbd68cf8ff70b4cf43af32461b56
raghav582 <raghavsharma582@gmail.com>
2020-06-02 18:47:19 +0530

    commit

--- stock_market.py
+++ stock_market.py
@@ -10,3 +10,7 @@ import datetime
 
 BA_obj = Browser_Automation()
 BA_obj.load_browser()
+today_date = str(datetime.date.today())
+data_path = "datasets/MarketWatch_"+today_date.split("-")[2]+"_"+today_date.split("-")[1]+"_"+today_date.split("-")[0]+"_lastupdatedon_04_00 PM.csv"
+data = pd.read_csv(data_path)
+data.to_csv('datasets/'+today_date+'.csv')
