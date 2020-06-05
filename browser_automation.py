fbc79cb96ea47ab7e30241fd54c9f2fb58dd8752
raghav582 <raghavsharma582@gmail.com>
2020-06-05 11:12:44 +0530

    commit

--- browser_automation.py
+++ browser_automation.py
@@ -5,12 +5,13 @@ Created on Fri May  8 15:00:07 2020
 
 @author: raghav
 """
-
+from kombu.utils.eventio import _select
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as waitE
+from selenium.webdriver.support.ui import Select
 import time
 import pandas as pd
 
@@ -19,6 +20,7 @@ class Browser_Automation:
     def __init__(self):
         self.equity_data = pd.DataFrame()
         self.browser = None
+        self.datapath = None
 
     def load_browser(self):
         options = webdriver.chrome.options.Options()
@@ -50,10 +52,22 @@ class Browser_Automation:
         self.browser_refresh()
         # while(waitE.visibility_of_element_located((By.TAG_NAME, 'table'))):
         #     time.sleep(2)
-        equity = self.browser.find_element_by_xpath('//*[@id="ContentPlaceHolder1_imgDownload"]')
-        equity.click()
+        index = self.browser.find_element_by_id("ContentPlaceHolder1_ddlType")
+        select_source = Select(index)
+        select_source.select_by_value("Index")
+
+        sandp = self.browser.find_element_by_id("ContentPlaceHolder1_ddlIndx")
+        select_source = Select(sandp)
+        select_source.select_by_value("98")
+
+        download = self.browser.find_element_by_id("ContentPlaceHolder1_imgDownload")
+        download.click()
+
 
     def browser_refresh(self):
         time.sleep(2)
         self.browser.delete_all_cookies()
         self.browser.refresh()
+
+    def getDatapath(self):
+        return self.datapath
--- stock_market.py
+++ stock_market.py
@@ -10,7 +10,8 @@ import datetime
 
 BA_obj = Browser_Automation()
 BA_obj.load_browser()
-today_date = str(datetime.date.today())
-data_path = "datasets/MarketWatch_"+today_date.split("-")[2]+"_"+today_date.split("-")[1]+"_"+today_date.split("-")[0]+"_lastupdatedon_04_00 PM.csv"
-data = pd.read_csv(data_path)
-data.to_csv('datasets/'+today_date+'.csv')
+# today_date = str(datetime.date.today()).split("-")
+# current_time = str(datetime.time).split(":")
+# data_path = "datasets/MarketWatch_"+today_date[2]+"_"+today_date[1]+"_"+today_date[0]+"_lastupdatedon_"+current_time[0]+"_"+00 PM.csv"
+# data = pd.read_csv(data_path)
+# data.to_csv('datasets/'+today_date+'.csv')
